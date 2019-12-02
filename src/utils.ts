02577912a53bb349a46b328f1de1be3653d9163a
Mike Cann <mike.cann@gmail.com>
2019-12-02 11:07:11 +0800

    not in node_modules

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,11 @@ import * as Path from "path";
 
 export const uriToFilename = (uri: vscode.Uri) => Path.basename(uri.path);
 
+export const uriToPath = (uri: vscode.Uri) => uri.path;
+
 export const startsWithCapitalLetter = (str: string) => {
   if (str.length == 0) return false;
   return str[0] == str[0].toUpperCase();
 };
+
+export const isInNodeModules = (path: string) => path.includes("node_modules");
